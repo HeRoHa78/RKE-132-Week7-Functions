426fef9bf458cf489ee7188619b81ab3534724af
Helvi <helviroman@gmail.com>
2023-11-03 15:37:45 +0200

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); //userInput -argument
+
+
+static void PrintAnyWord (string anystring) // anyString -parameter
+{
+    anystring = anystring.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anystring);
+    }
+}
